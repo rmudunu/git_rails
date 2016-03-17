58990e01d49b6e9f997da0aef699b49a0d681dce
Raju <ramachandraraju92@gmail.com>
2016-03-17 12:36:20 -0400

    modified wordcount to have a method

--- Desktop/hw1.rb
+++ Desktop/hw1.rb
@@ -1,6 +1,10 @@
-puts "Enter the sentence"
+def wordcount
+ puts "Enter the sentence"
  a = gets.to_s
-words = a.split(' ')
-count = Hash.new(0)
-words.each { |word| count[word.downcase] += 1 }
-puts count
+ words = a.split(' ')
+ count = Hash.new(0)
+ words.each { |word| count[word.downcase] += 1 }
+ puts count
+end
+
+wordcount 
